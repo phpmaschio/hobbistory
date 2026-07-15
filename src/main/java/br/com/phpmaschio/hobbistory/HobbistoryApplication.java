bc0ef9aaaa36715b192bf815a3546e5aa80afe78
Pedro Maschio <pedrohenriquemaschio@gmail.com>
2026-07-15 13:20:01 -0300

    criação do projeto

---
+++ src/main/java/br/com/phpmaschio/hobbistory/HobbistoryApplication.java
@@ -0,0 +1,13 @@
+package br.com.phpmaschio.hobbistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/phpmaschio/hobbistory/HobbistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phpmaschio.hobbistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
